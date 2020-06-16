72d3645ea3f0b6961f256ac3f5ec6a42d256eec6
Aleksandr Zarubin <aljkeo.O@gmail.com>
2020-06-16 14:27:28 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int refill = 100;
+        int refill = 1000;
         int bonus;
-        if (refill >=1000) {
+        if (refill >= 1000) {
             bonus = refill / 100;
         } else {
             bonus = 0;
